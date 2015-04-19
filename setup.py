a4d443ced850bb500f0815d3dd30308ec4756164
Josef Fruehwald <JoFrhwld@gmail.com>
2015-04-19 11:30:20 +0100

    skeleton

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='praatclasses',
+      version='0.1',
+      description='A bunch of praat object classes for python',
+      url='http://github.com/jofrhwld/praatclasses',
+      author='Ingrid Rosenfelder',
+      maintainer = 'Josef Fruehwald',
+      maintainer_email = 'jofrhwld@gmail.com',
+      license='MIT',
+      packages=['praatclasses'],
+      zip_safe=False)
